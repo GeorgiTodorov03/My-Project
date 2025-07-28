f3c728dc240e750c4683b435f548f3410ddb8580
GeorgiTodorov03 <joro_t03@abv.bg>
2025-07-28 16:51:03 +0300

    Fixed errors and added resource folder with labels

--- MyShop/src/MyShop/Menu/Menu.java
+++ MyShop/src/MyShop/Menu/Menu.java
@@ -2,7 +2,7 @@ package MyShop.Menu;
 
 public interface Menu {
 
-    String RESOURCE_BUNDLE_BASE_NAME = "MyShop.labels";
+    String RESOURCE_BUNDLE_BASE_NAME = "shop.labels";
 
     void start();
 
